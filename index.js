882de7fb28cb0b513ab6939f9ab037f9867a3d9a
ParadiseLenkon <134364336+ParadiseLenkon@users.noreply.github.com>
2023-07-21 14:12:11 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,228 @@
+let map;
+let places;
+let infoWindow;
+let markers = [];
+let autocomplete;
+const countryRestrict = { country: "jp" };
+const MARKER_PATH =
+  "https://developers.google.com/maps/documentation/javascript/images/marker_green";
+const hostnameRegexp = new RegExp("^https?://.+?/");
+const countries = {
+  jp: {
+    center: { lat: 35.6994976345537,  lng: 139.6982021475387},
+    zoom: 5,
+  },
+};
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: countries["jp"].zoom,
+    center: countries["jp"].center,
+    mapTypeControl: false,
+    panControl: false,
+    zoomControl: false,
+    streetViewControl: false,
+  });
+  infoWindow = new google.maps.InfoWindow({
+    content: document.getElementById("info-content"),
+  });
+
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("autocomplete"),
+    {
+      types: ["(cities)"],
+      componentRestrictions: countryRestrict,
+      fields: ["geometry"],
+    }
+  );
+  places = new google.maps.places.PlacesService(map);
+  autocomplete.addListener("place_changed", onPlaceChanged);
+  
+  document
+    .getElementById("country")
+    .addEventListener("change", setAutocompleteCountry);
+}
+
+function onPlaceChanged() {
+  const place = autocomplete.getPlace();
+
+  if (place.geometry && place.geometry.location) {
+    map.panTo(place.geometry.location);
+    map.setZoom(15);
+    search();
+  } else {
+    document.getElementById("autocomplete").placeholder = "Enter a city";
+  }
+}
+
+function search() {
+  const search = {
+    bounds: map.getBounds(),
+    types: ["lodging"],
+  };
+
+  places.nearbySearch(search, (results, status, pagination) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      clearResults();
+      clearMarkers();
+
+      
+      for (let i = 0; i < results.length; i++) {
+        const markerLetter = String.fromCharCode("A".charCodeAt(0) + (i % 26));
+        const markerIcon = MARKER_PATH + markerLetter + ".png";
+
+        
+        markers[i] = new google.maps.Marker({
+          position: results[i].geometry.location,
+          animation: google.maps.Animation.DROP,
+          icon: markerIcon,
+        });
+        
+        markers[i].placeResult = results[i];
+        google.maps.event.addListener(markers[i], "click", showInfoWindow);
+        setTimeout(dropMarker(i), i * 100);
+        addResult(results[i], i);
+      }
+    }
+  });
+}
+
+function clearMarkers() {
+  for (let i = 0; i < markers.length; i++) {
+    if (markers[i]) {
+      markers[i].setMap(null);
+    }
+  }
+
+  markers = [];
+}
+
+
+function setAutocompleteCountry() {
+  const country = document.getElementById("country").value;
+
+  if (country == "all") {
+    autocomplete.setComponentRestrictions({ country: [] });
+    map.setCenter({ lat: 15, lng: 0 });
+    map.setZoom(2);
+  } else {
+    autocomplete.setComponentRestrictions({ country: country });
+    map.setCenter(countries[country].center);
+    map.setZoom(countries[country].zoom);
+  }
+
+  clearResults();
+  clearMarkers();
+}
+
+function dropMarker(i) {
+  return function () {
+    markers[i].setMap(map);
+  };
+}
+
+function addResult(result, i) {
+  const results = document.getElementById("results");
+  const markerLetter = String.fromCharCode("A".charCodeAt(0) + (i % 26));
+  const markerIcon = MARKER_PATH + markerLetter + ".png";
+  const tr = document.createElement("tr");
+
+  tr.style.backgroundColor = i % 2 === 0 ? "#F0F0F0" : "#FFFFFF";
+  tr.onclick = function () {
+    google.maps.event.trigger(markers[i], "click");
+  };
+
+  const iconTd = document.createElement("td");
+  const nameTd = document.createElement("td");
+  const icon = document.createElement("img");
+
+  icon.src = markerIcon;
+  icon.setAttribute("class", "placeIcon");
+  icon.setAttribute("className", "placeIcon");
+
+  const name = document.createTextNode(result.name);
+
+  iconTd.appendChild(icon);
+  nameTd.appendChild(name);
+  tr.appendChild(iconTd);
+  tr.appendChild(nameTd);
+  results.appendChild(tr);
+}
+
+function clearResults() {
+  const results = document.getElementById("results");
+
+  while (results.childNodes[0]) {
+    results.removeChild(results.childNodes[0]);
+  }
+}
+
+
+function showInfoWindow() {
+  
+  const marker = this;
+
+  places.getDetails(
+    { placeId: marker.placeResult.place_id },
+    (place, status) => {
+      if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        return;
+      }
+
+      infoWindow.open(map, marker);
+      buildIWContent(place);
+    }
+  );
+}
+
+
+function buildIWContent(place) {
+  document.getElementById("iw-icon").innerHTML =
+    '<img class="hotelIcon" ' + 'src="' + place.icon + '"/>';
+  document.getElementById("iw-url").innerHTML =
+    '<b><a href="' + place.url + '">' + place.name + "</a></b>";
+  document.getElementById("iw-address").textContent = place.vicinity;
+  if (place.formatted_phone_number) {
+    document.getElementById("iw-phone-row").style.display = "";
+    document.getElementById("iw-phone").textContent =
+      place.formatted_phone_number;
+  } else {
+    document.getElementById("iw-phone-row").style.display = "none";
+  }
+
+  
+  if (place.rating) {
+    let ratingHtml = "";
+
+    for (let i = 0; i < 5; i++) {
+      if (place.rating < i + 0.5) {
+        ratingHtml += "&#10025;";
+      } else {
+        ratingHtml += "&#10029;";
+      }
+
+      document.getElementById("iw-rating-row").style.display = "";
+      document.getElementById("iw-rating").innerHTML = ratingHtml;
+    }
+  } else {
+    document.getElementById("iw-rating-row").style.display = "none";
+  }
+
+  
+  if (place.website) {
+    let fullUrl = place.website;
+    let website = String(hostnameRegexp.exec(place.website));
+
+    if (!website) {
+      website = "http://" + place.website + "/";
+      fullUrl = website;
+    }
+
+    document.getElementById("iw-website-row").style.display = "";
+    document.getElementById("iw-website").textContent = website;
+  } else {
+    document.getElementById("iw-website-row").style.display = "none";
+  }
+}
+
+window.initMap = initMap;
